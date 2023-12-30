3e791d332d870a43b458e5a19ce1a628b26eb7b8
HarrtyXX <ddominik.kk123@wp.pl>
2023-12-31 00:43:43 +0100

    Changed index view to search for todolist

--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Hello world!</h1>")
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    item = ls.item_set.get(id=1)
+    return HttpResponse(f"<h1>{ls.name}</h1> <h3>{item.text}</h3>")
 
-def v1(response):
-    return HttpResponse("<h1>View 1!</h1>")
